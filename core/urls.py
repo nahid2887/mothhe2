919f01358ba698f21c9a8264001970c6556f8d1e
nahid2887 <nahid2887@gmail.com>
2026-01-19 10:29:58 +0600

    add

--- core/urls.py
+++ core/urls.py
@@ -46,3 +46,4 @@ urlpatterns += [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+# Serve static and media files during development
